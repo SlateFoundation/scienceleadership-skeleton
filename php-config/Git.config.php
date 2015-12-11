d2346c1ecb2f69d60f379e439fff0a8389b55e6c
Chris Alfano <chris@jarv.us>
2015-12-11 21:28:01 +0000

    Add slate-backup repo config

--- php-config/Git.config.php
+++ php-config/Git.config.php
@@ -25,3 +25,22 @@ Git::$repositories['scienceleadership-sbg'] = [
         'site-root/sbg'
     ]
 ];
+
+Git::$repositories['slate-backup'] = [
+    'remote' => 'git@github.com:JarvusInnovations/slate-backups.git',
+    'originBranch' => 'releases/v1',
+    'workingBranch' => Site::getConfig('primary_hostname'),
+    'localOnly' => true,
+    'trees' => [
+        'ext-library',
+        'html-templates',
+        'php-classes',
+        'php-config',
+        'php-migrations',
+        'phpunit-tests',
+        'sencha-build',
+        'sencha-workspace',
+        'sencha-docs',
+        'site-root'
+    ],
+];
