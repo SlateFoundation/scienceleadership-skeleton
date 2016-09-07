568c6ec4501dacdcffa54a550e1683a394376eb0
Christian Kunkel <christian@jarv.us>
2016-09-07 18:13:15 -0400

    Add student tool links

--- php-config/Slate/UI/Tools.config.d/sla-students.php
+++ php-config/Slate/UI/Tools.config.d/sla-students.php
@@ -5,4 +5,9 @@ if (empty($GLOBALS['Session']) || $GLOBALS['Session']->hasAccountLevel('Staff'))
 }
 
 Slate\UI\Tools::$tools['Naviance'] = 'https://www.philasd.org/login/';
-Slate\UI\Tools::$tools['SchoolNet'] = 'https://www.philasd.org/login/';
+Slate\UI\Tools::$tools['StudentNet'] = 'https://www.philasd.org/login/';
+Slate\UI\Tools::$tools['RosettaStone'] = 'https://philadelphiasd.rosettastoneclassroom.com/en-US/portal';
+Slate\UI\Tools::$tools['College Board'] = 'https://www.collegeboard.org/?navid=gh-cb';
+Slate\UI\Tools::$tools['Scrible'] = 'https://www.scrible.com/sign-in/#/';
+Slate\UI\Tools::$tools['College News Blog'] = 'https://slacollegenews.wordpress.com/';
+Slate\UI\Tools::$tools['Lucid Press'] = 'https://www.lucidpress.com/users/login';
