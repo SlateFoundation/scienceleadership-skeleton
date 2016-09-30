d57ae62bf41af96ff0ad4a32d1d51ba69a9cf291
Chris Alfano <chris@jarv.us>
2016-09-30 09:51:14 -0400

    Tweak links

--- php-config/Slate/DashboardRequestHandler.config.d/800_sla-teachers.php
+++ php-config/Slate/DashboardRequestHandler.config.d/800_sla-teachers.php
@@ -10,9 +10,13 @@ Slate\DashboardRequestHandler::$sources['sla-teachers'] = [
 #       ,'Teacher Dashboard' => 'https://teacherdashboard.appspot.com/' . GoogleApps::$domain,
         'Naviance' => 'https://succeed.naviance.com/auth/signin',
         'Login SDP' => 'https://www.philasd.org/login/',
-        'Zimbra - SDP email' => 'https://cc.philasd.org/',
+        'Zimbra' => 'https://cc.philasd.org/',
         'SDP Report Card Calendar' => 'http://webgui.phila.k12.pa.us/offices/r/report-card-information',
         'Wifi Login' => 'https://nps-sth1.philasd.org',
+        'Create an Account' => 'https://sites.google.com/a/scienceleadership.org/sla-account-new-user-request/',
+        'EduCon' => 'http://educonphilly.org/',
+        'Scrible' => 'https://www.scrible.com/sign-in/#/',
+
 
     ]
 ];
