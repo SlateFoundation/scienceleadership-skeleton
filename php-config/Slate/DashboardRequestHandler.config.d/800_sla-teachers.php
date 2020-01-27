41641cb5a631c5fe49fa4309ba3d6ae77548b607
Chris Alfano <chris@jarv.us>
2020-01-27 02:29:35 +0000

    fix: update naviance link

--- php-config/Slate/DashboardRequestHandler.config.d/800_sla-teachers.php
+++ php-config/Slate/DashboardRequestHandler.config.d/800_sla-teachers.php
@@ -8,7 +8,7 @@ if (empty($GLOBALS['Session']) || !$GLOBALS['Session']->hasAccountLevel('Staff')
 Slate\DashboardRequestHandler::$sources['sla-teachers'] = [
     'For Teachers' => [
 #       ,'Teacher Dashboard' => 'https://teacherdashboard.appspot.com/' . GoogleApps::$domain,
-        'Naviance' => 'https://succeed.naviance.com/auth/signin',
+        'Naviance' => 'https://id.naviance.com/',
         'Login SDP' => 'https://www.philasd.org/login/',
         'Zimbra' => 'https://cc.philasd.org/',
         'SDP Report Card Calendar' => 'http://webgui.phila.k12.pa.us/offices/r/report-card-information',
